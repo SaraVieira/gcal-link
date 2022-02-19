bb340947f139aec19556b6e601d847e01d699eeb
Sara Vieira <hey@iamsaravieira.com>
2022-02-19 19:36:23 +0100

    fix styled

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
+import "../style.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body
-        style={{
-          lineHeight: "1.5",
-          color: "rgba(255, 255, 255, 0.92)",
-          backgroundColor: "rgb(26, 32, 44)",
-        }}
-      >
+      <body>
         <Main />
         <NextScript />
         <script
