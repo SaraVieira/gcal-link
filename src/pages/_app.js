b56ed0dd17b1ddc51779efd80c0f256cd4423ec3
Sara Vieira <hey@iamsaravieira.com>
2022-02-19 19:17:01 +0100

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
         <title>Google Calendar Link Generator</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📆</text></svg>"
+        ></link>
         <meta
           name="description"
           content="Easily generate sharable google calendar links"
